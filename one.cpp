934b2dac7055b955f6bcfe57cbb090510e5e8d0b
cocainedentist <cocainedentist0@gmail.com>
2025-09-22 11:05:51 +0900

    five stars

--- one.cpp
+++ one.cpp
@@ -3,12 +3,15 @@
 using namespace std;
 
 int main() {
-	int result1 = 0;
-	int result2 = 0;
+	int num[10] = { 0, };
 
-	for (int i = 1; i <= 100; i++) result1 += i;
-	for (int i = 3; i <= 100; i = i+3) result2 += i;
+	for (int i = 6; i > 0; --i) {
+		for (int j = 6; j > 0; --j) {
+			if (i <= j) cout << " ";
+			else cout << "*";
+		}
+		cout << endl;
+	}
 
-	cout << "1부터 100까지의 합 : " << result1 << endl;
-	cout << "1부터 100까지 3의 배수 합 : " << result2 << endl;
+	return 0;
 }
