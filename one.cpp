aba85fb6c312a62435bc948211e0a8417548551c
cocainedentist <cocainedentist0@gmail.com>
2025-09-22 09:36:43 +0900

    1.0

--- one.cpp
+++ one.cpp
@@ -7,7 +7,7 @@ int main() {
 	int result2 = 0;
 
 	for (int i = 1; i <= 100; i++) result1 += i;
-	for (int i = 1; i <= 100; i++) if (i % 3 == 0) result2 += i;
+	for (int i = 3; i <= 100; i = i+3) result2 += i;
 
 	cout << "1부터 100까지의 합 : " << result1 << endl;
 	cout << "1부터 100까지 3의 배수 합 : " << result2 << endl;
